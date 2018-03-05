0b83ed9fef0cb53f6b68f73a23d7a425fec72a0f
KEVIN-ORTIZ\Personal <khanzdi@unac.edu.co>
2018-03-05 15:19:03 -0500

    Firebase Settings

--- app/src/main/java/com/example/brayandavid/homemedicines/MainActivity.java
+++ app/src/main/java/com/example/brayandavid/homemedicines/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.brayandavid.homemedicines;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
 }
