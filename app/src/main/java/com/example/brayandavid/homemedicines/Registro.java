3631e1264b812bee32ffdaf66e960bf799a29882
User-PC\CAT <jksolarte21@gmail.com>
2018-03-17 12:21:54 -0500

    Revert "Changes - Layouts - Logical login"
    This reverts commit 8f284ddba6b7a58a13e28507f683874400f3d997.

--- app/src/main/java/com/example/brayandavid/homemedicines/Registro.java
+++ app/src/main/java/com/example/brayandavid/homemedicines/Registro.java
@@ -65,7 +65,7 @@ public class Registro extends AppCompatActivity {
         int code = TaskUserRegistry.getCode();
         if (code == 200) {
             progressDialog = ProgressDialog.show(this, "Congratulations", "Registro exitoso, ahora puedes ingresar",false );
-            Intent i = new Intent(Registro.this, LoginUser.class);
+            Intent i = new Intent(Registro.this, ServicesListActivity.class);
             startActivity(i);
             finish();
         }else {
