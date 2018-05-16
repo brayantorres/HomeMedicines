40c41e8bce75af6b3d1c5e5fbf9d601a7e4b088e
brayantorres <brayantorres9431@gmail.com>
2018-05-16 14:41:49 -0500

    Agrego SearchView con opciones de busqueda, settings, about y help

--- app/src/main/java/com/example/brayandavid/homemedicines/View/ServicesListActivity.java
+++ app/src/main/java/com/example/brayandavid/homemedicines/View/ServicesListActivity.java
@@ -6,14 +6,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.brayandavid.homemedicines.Conection.TaskImages;
 import com.example.brayandavid.homemedicines.Conection.TaskProducts;
@@ -26,6 +32,7 @@ import java.util.List;
 
 public class ServicesListActivity extends AppCompatActivity {
 
+    TextView textView;
     private boolean mTwoPane;
 
     @Override
@@ -33,6 +40,7 @@ public class ServicesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicios_list);
 
+        textView = (TextView) findViewById(R.id.textViewSearch);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
@@ -137,7 +145,51 @@ public class ServicesListActivity extends AppCompatActivity {
         }
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint(getText(R.string.search));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(ServicesListActivity.this, R.string.submitted, Toast.LENGTH_SHORT).show();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                textView.setText(newText);
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean OnOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                action(R.string.action_settings);
+                return true;
+            case R.id.action_help:
+                action(R.string.action_help);
+                return true;
+            case R.string.action_about:
+                action(R.string.action_about);
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void action(int resid) {
+        Toast.makeText(this, getText(resid), Toast.LENGTH_SHORT).show();
+    }
 
     public List<Product> getProductxxx() {
         List<Product> list = new ArrayList<>();
